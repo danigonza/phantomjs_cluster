971633f26292b0a8c21faf3619d69f0b042abbd0
Daniel Gonzalez <daniel@notegraphy.com>
2014-01-09 19:46:56 +0100

    Adding redis

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 var config = require('config');
 var express = require('express');
 var RasterizerService = require('./lib/rasterizerService');
+var RedisService = require('./lib/redisService');
 
 process.on('uncaughtException', function (err) {
   console.error("[uncaughtException]", err);
@@ -20,13 +21,20 @@ process.on('SIGINT', function () {
 
 // web service
 var app = express();
-app.configure(function(){
-  app.use(app.router);
-  app.set('rasterizerService', new RasterizerService(config.rasterizer).startService());
-});
-app.configure('development', function() {
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-require('./routes')(app, config.server.useCors);
-app.listen(config.server.port);
-console.log('Express server listening on port ' + config.server.port);
+var redisService = new RedisService(config.redis).startService()
+setTimeout(function(){
+	app.configure(function(){
+	  app.use(app.router);
+	  app.set('redisService', redisService);
+	  for (var i=0; i < config.rasterizer.num; i++){
+	  	app.set('rasterizerService_' + (config.rasterizer.port + i), new RasterizerService(config.rasterizer, config.rasterizer.port + i, redisService).startService());
+	  }
+	});
+	app.configure('development', function() {
+	  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+	});
+	require('./routes')(app, config.server.useCors);
+	app.listen(config.server.port);
+	console.log('Express server listening on port ' + config.server.port);
+}, 100);
+
